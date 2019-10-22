100824f08ad91c591e3f98b5e32624f7ce9a7a5a
Adam Roberts <aroberts@uk.ibm.com>
2019-10-22 16:04:51 +0100

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -11,11 +11,11 @@ read -p "Press enter to continue - I've installed the ServiceMesh operator"
 oc new-project istio-system
 oc apply -f MaistraFile.yaml
 sleep 5
-oc apply -f MemberRole.yaml
+oc apply -f MemberRoll.yaml
 
 echo "Install the Tekton Pipelines operator AND an instance before you continue"
 
-read -p "Press enter to continue - I've installed the Tekton Pipelines operator AND an instance of Tekton Pipelines"
+read -p "Press enter to continue - I've installed the Tekton Pipelines operator"
 
 echo "Install the Knative Eventing 0.8 operator"
 
@@ -25,34 +25,11 @@ echo "Installing the Knative Eventing Contrib GitHubSource CRD"
 
 oc apply -f https://github.com/knative/eventing-contrib/releases/download/v0.8.0/github.yaml
 
-echo "Installing the Serverless Operator 1.1.0 from GitHub: https://github.com/openshift-knative/serverless-operator"
+echo "Install the Serverless 1.1.0 operator"
 
-rm -rf ./serverless-operator
-git clone https://github.com/openshift-knative/serverless-operator.git
-cd serverless-operator
-./hack/catalog.sh | kubectl apply -n openshift-marketplace -f -
-
-OPERATOR_NS=$(kubectl get og --all-namespaces | grep global-operators | awk '{print $1}')
-
-cat <<-EOF | kubectl apply -f -
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: serverless-operator-sub
-  generateName: serverless-operator-
-  namespace: $OPERATOR_NS
-spec:
-  source: serverless-operator
-  sourceNamespace: openshift-marketplace
-  name: serverless-operator
-  channel: techpreview
-EOF
-
-sleep 5
+read -p "Press enter to continue - I've installed the Serverless operator"
 
 echo "Installing Knative Serving object"
-
-cd ../
 
 oc apply -f Serving.yaml
 
@@ -72,9 +49,6 @@ echo "Done!"
 
 echo "Post install notes you should be aware of"
 
-echo "webhooks-route not needed (mentioned in our instructions so far for webhooks extension)"
-echo "configmap already set (mentioned in our instructions so far for webhooks extension)"
-echo "no need for those oc router commands (mentioned in our instructions so far for webhooks extension)"
 echo "member roll super important - ensure namespaces you want serverless stuff to be in, are mentioned in the roll"
 echo "describe your virtualservices if your ksvc isn't ready"
 echo "networking pod in istio-system for debugging"
